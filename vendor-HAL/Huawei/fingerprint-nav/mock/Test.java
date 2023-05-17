07e96411ea0c3b59d00d6feb6384973acde85d63
ac973k <daniil@terentyeff.ru>
2023-05-17 23:32:48 +0300

    erase

--- vendor-HAL/Huawei/fingerprint-nav/mock/Test.java
+++
@@ -1,8 +0,0 @@
-import vendor.huawei.hardware.biometrics.fingerprint.V2_1.IExtBiometricsFingerprint;
-public class Test {
-	static public void main(String[] args) {
-		System.out.println("Hello");
-		IExtBiometricsFingerprint service = IExtBiometricsFingerprint.getService();
-		service.sendCmdToHal(0x29);
-	}
-};
